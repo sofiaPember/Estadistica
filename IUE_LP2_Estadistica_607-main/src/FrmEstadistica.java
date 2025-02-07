a86830948a7e4326292dbc0d0a18ab76464e22fc
salas <salas@salas.local>
2025-02-07 13:59:51 -0500

    se agrega evento a la GUI

--- IUE_LP2_Estadistica_607-main/src/FrmEstadistica.java
+++ IUE_LP2_Estadistica_607-main/src/FrmEstadistica.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -5,6 +8,7 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
@@ -56,6 +60,31 @@ public class FrmEstadistica extends JFrame {
         JTextField txtEstadistica = new JTextField();
         txtEstadistica.setBounds(210, 200, 100, 25);
         getContentPane().add(txtEstadistica);
+
+        btnAgregar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agregarDato();
+            }
+        });
+
+        btnQuitar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                quitarDato();
+            }
+        });
+
+    }
+
+    private void agregarDato() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en Agregar");
+
+    }
+
+    private void quitarDato() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en Quitar");
+
     }
 
 }
